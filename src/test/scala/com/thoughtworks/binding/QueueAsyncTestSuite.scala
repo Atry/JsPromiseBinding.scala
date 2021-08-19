9c5bcfb30b6382862bf815078533b450f988dafa
Scala Steward <me@scala-steward.org>
2021-08-19 08:13:13 +0200

    Reformat with scalafmt 3.0.0

--- src/test/scala/com/thoughtworks/binding/QueueAsyncTestSuite.scala
+++ src/test/scala/com/thoughtworks/binding/QueueAsyncTestSuite.scala
@@ -1,7 +1,7 @@
 package com.thoughtworks.binding
 
-/**
-  * @author 杨博 (Yang Bo)
+/** @author
+  *   杨博 (Yang Bo)
   */
 trait QueueAsyncTestSuite extends org.scalatest.AsyncTestSuite {
   override implicit def executionContext = scala.scalajs.concurrent.JSExecutionContext.queue
